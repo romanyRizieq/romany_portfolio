54db6060796de221108e56b3291fb8134ab17d25
Romany Rizieq <romanyfarouk9999@gmail.com>
2024-12-07 07:50:55 +0200

    change path file

--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,21 @@
 /**
  * Node modules
  */
-// import React from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Import createRoot from react-dom/client
+import { BrowserRouter as Router } from 'react-router-dom'; // Import the Router component for routing context
 import App from './App'; // Import your main App component
 import './index.css'; // Ensure Tailwind CSS is imported
 
 // Find the root element in your HTML
 const rootElement = document.getElementById('root');
 
-// Create a root and render the App component
+// Create a root and render the App component wrapped in BrowserRouter for routing context
 const root = ReactDOM.createRoot(rootElement); 
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> {/* Wrap the App component inside Router */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
-
