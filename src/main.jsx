b0d96acf457252ef8a136f137848aac173aacf72
Romany Rizieq <romanyfarouk9999@gmail.com>
2024-12-07 06:33:17 +0200

    change path file

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@
 // import React from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Import createRoot from react-dom/client
+import { BrowserRouter } from "react-router-dom";
 import App from './App'; // Import your main App component
 import './index.css'; // Ensure Tailwind CSS is imported
 
